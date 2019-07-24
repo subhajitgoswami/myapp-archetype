8ea6253c4c7a48eedcf885d5cbf20eb45827cc19
SubhajitGoswami <subhajitbumba697@gmail.com>
2019-07-25 02:48:23 +0530

    intial commit

---
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/__project-name__.java
@@ -0,0 +1,21 @@
+package ${package}.${artifactId};
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableRetry
+public class $ {
+	project-name}Application{
+
+	public static void main(String[] args) {
+		final SpringApplication springApplication = new SpringApplication(Application.class);
+	}
+
+}
